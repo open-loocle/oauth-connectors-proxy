ffa048a792bc60b59b65dcd726619afc9c390550
Sergey Novikov <sergey.novikov@skyscanner.net>
2021-02-14 00:33:26 +0100

    Handle lambdaLocal.execute errors

--- devServer.ts
+++ devServer.ts
@@ -1,4 +1,6 @@
 // Taken from https://steveholgado.com/aws-lambda-local-development/
+import { APIGatewayProxyResult } from 'aws-lambda';
+
 const express = require('express');
 const path = require('path');
 const lambdaLocal = require('lambda-local');
@@ -18,17 +20,23 @@ app.use('/connectors/:file/:handler', async (req, res) => {
     body: req.body,
   };
   console.debug('event', event);
-  const result = await lambdaLocal.execute({
-    lambdaPath: path.join(__dirname, 'connectors', req.params['file']),
-    lambdaHandler: req.params['handler'],
-    envfile: path.join(__dirname, '.env.dev'),
-    event,
-    timeoutMs: 30000,
-  });
-  res
-  .status(result.statusCode)
-  .set(result.headers)
-  .end(result.body);
+  let result: APIGatewayProxyResult;
+  try {
+    result = await lambdaLocal.execute({
+      lambdaPath: path.join(__dirname, 'connectors', req.params['file']),
+      lambdaHandler: req.params['handler'],
+      envfile: path.join(__dirname, '.env.dev'),
+      event,
+      timeoutMs: 30000,
+    });
+    res
+    .status(result.statusCode)
+    .set(result.headers)
+    .end(result.body);
+  } catch (e) {
+    console.error(e);
+    res.status(500).end(JSON.stringify({error: e}));
+  }
 });
 
 app.listen(3000, () => console.log('listening on port: 3000'));
